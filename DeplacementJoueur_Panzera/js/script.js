f1e3b77b9b6e1c254b3329e1d77b42285ec20e78
Alexis PANZERA <alexis.panzera@etu.unice.fr>
2018-11-16 22:29:07 +0100

    Ajout monter et descendre cheval avec barre espace

--- DeplacementJoueur_Panzera/js/script.js
+++ DeplacementJoueur_Panzera/js/script.js
@@ -4,10 +4,122 @@ let keyd, keyu;
 let rigthEna, leftEna, upEna, downEna;
 let mousePos;
 let fondEna = 0;
+let chevalEna = 0;
+let chevalApp = 0;
 var personnage = new Image();
 personnage.src = "personnage.png";
+var cheval = new Image();
+cheval.src = "cheval.png";
 var fond = new Image();
 fond.src = "fond.jpg";
+
+class PersonnageI{
+
+  constructor(x, y, l, h, v, vy, angle) {
+    this.x = x;
+    this.y = y;
+    this.l = l;
+    this.h = h;
+    this.v = v;
+    this.vy = vy;
+    this.angle = angle || 0;
+  }
+
+draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.translate(-100/2, -186/2);
+    ctx.drawImage(personnage, 0, 0, 100, 186);
+    ctx.restore();
+ 
+  }
+
+  suitsouris(mousePos)
+  {
+    let dx = this.x - mousePos.x;
+    let dy = this.y - mousePos.y;
+    this.angle = Math.atan2(dy,dx) -Math.PI/2;
+    
+  }
+
+  movel()
+  {
+    this.x -= this.v;
+  }
+
+  mover()
+  {
+    this.x += this.v;
+  }
+
+  moveu()
+  {
+    this.y -= this.v;
+  }
+
+  moved()
+  {
+    this.y += this.v;
+  }
+
+
+
+}
+
+class ChevalI{
+
+  constructor(x, y, l, h, v, vy, angle) {
+    this.x = x;
+    this.y = y;
+    this.l = l;
+    this.h = h;
+    this.v = v;
+    this.vy = vy;
+    this.angle = angle || 0;
+  }
+
+draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.translate(-100/2, -186/2);
+    ctx.drawImage(cheval, 0, 0, 100, 186);
+    ctx.restore();
+ 
+  }
+
+  suitsouris(mousePos)
+  {
+    let dx = this.x - mousePos.x;
+    let dy = this.y - mousePos.y;
+    this.angle = Math.atan2(dy,dx) -Math.PI/2;
+    
+  }
+
+  movel()
+  {
+    this.x -= this.v;
+  }
+
+  mover()
+  {
+    this.x += this.v;
+  }
+
+  moveu()
+  {
+    this.y -= this.v;
+  }
+
+  moved()
+  {
+    this.y += this.v;
+  }
+
+
+
+}
 
 class Rectangle {
   constructor(x, y, l, h, vx, vy, angle) {
@@ -93,6 +205,10 @@ window.onload = function () {
     hc = canvas.height;
     ctx = canvas.getContext("2d");
     creerDesRectangles();
+    creerPersonnages();
+    //creerCheval();
+
+
   
     // Pour animation à 60 im/s
     
@@ -101,14 +217,40 @@ window.onload = function () {
 personnage.onload = function()
 {
 
-  requestAnimationFrame(anime);
+  
 
 }
 
 fond.onload = function()
 {
+requestAnimationFrame(anime);
+//fondEna = 1;
 
-fondEna = 1;
+}
+function creerCheval()
+{
+
+
+
+}
+function creerPersonnages()
+{
+
+   let l = 60;
+    let h = 60;
+    let x = Math.random() * (lc - 60);
+    let y = Math.random()*(hc - 60);
+    let v = 5;
+    let vy = 0;
+  p1 = new PersonnageI(x, y, l, h, v, vy)
+
+      l = 60;
+     h = 60;
+      x = Math.random() * (lc - 60);
+      y = Math.random()*(hc - 60);
+
+     vy = 0;
+  ch1 = new ChevalI(x, y, l, h, v, vy)
 
 }
 
@@ -177,60 +319,93 @@ function onkeyu_page(event)
     rigthEna = 0;
     //  console.log("Up D");
   }
+
+  
 }
 
 function mouvementJoueur()
 {
   if(rigthEna)
   {
-     tableauDesRectangles.forEach((r) => {
-    r.mover(ctx);
-  })
+    p1.mover();
+
+  //    tableauDesRectangles.forEach((r) => {
+  //   r.mover(ctx);
+  // })
   }
   if(leftEna)
   {
-    tableauDesRectangles.forEach((r) => {
-    r.movel(ctx);
-  })
+    p1.movel();
+
+  //   tableauDesRectangles.forEach((r) => {
+  //   r.movel(ctx);
+  // })
   }
   if(upEna)
   {
-    tableauDesRectangles.forEach((r) => {
-    r.moveu(ctx);
-  })
+    p1.moveu();
+
+  //   tableauDesRectangles.forEach((r) => {
+  //   r.moveu(ctx);
+  // })
   }
   if(downEna)
   {
-    tableauDesRectangles.forEach((r) => {
-    r.moved(ctx);
-  })
+    p1.moved();
+
+  //   tableauDesRectangles.forEach((r) => {
+  //   r.moved(ctx);
+  // })
+  }
+}
+
+function mouvementCheval()
+{
+  if(rigthEna)
+  {
+
+    ch1.mover();
+  }
+  if(leftEna)
+  {
+
+    ch1.movel();
+  }
+  if(upEna)
+  {
+
+    ch1.moveu();
+  }
+  if(downEna)
+  {
+
+    ch1.moved();
   }
 }
 
 function detectMur()
 {
 
-  tableauDesRectangles.forEach((r) => {
-    if(r.x <= 5)
+
+    if(p1.x <= 5)
     {
       leftEna = 0;
     }
 
-    if(r.x >= (lc - r.l))
+    if(p1.x >= (lc))
     {
       rigthEna = 0;
     }
 
-    if(r.y <= 6)
+    if(p1.y <= 6)
     {
       upEna = 0;
     }
 
-    if(r.y >= hc - r.h)
+    if(p1.y >= hc)
     {
       downEna = 0;
     }
-  })
 
  
 }
@@ -258,6 +433,22 @@ function onkeyd_page(event)
   {
     rigthEna = 1;
   }
+  if(event.keyCode == 32 && chevalEna == 1)
+  {
+    chevalEna = 0;
+    ch1.x = p1.x - 15;
+    ch1.v = 5;
+  p1.v=5;
+  }
+}
+
+function dessinerPersonnage(){
+
+p1.draw(ctx);
+}
+
+function dessinerCheval(){
+  ch1.draw(ctx);
 }
 
 function dessinerLesRectangles() {
@@ -292,19 +483,50 @@ function changerTaille() {
 function anime() {
 
     ctx.clearRect(0, 0, lc, hc);
-    if(fondEna)
+
+ 
     ctx.drawImage(fond, 0, 0, lc, hc);
+  
 
     detectMur();
 
 if(mousePos !== undefined) {
-  tableauDesRectangles.forEach((r) => {
-    r.suitsouris(mousePos);
-  })
+  // tableauDesRectangles.forEach((r) => {
+  //   r.suitsouris(mousePos);
+  // })
+  p1.suitsouris(mousePos);
+  if (chevalEna)
+  ch1.suitsouris(mousePos);
 }
 
+
+//gerer les mouvements
     mouvementJoueur();
-    dessinerLesRectangles(); 
+    if (chevalEna)
+    {
+    mouvementCheval();
+  }
+
+
+//dessiner les personnges
+
+    dessinerCheval(); 
+    dessinerPersonnage();
+    
+if(p1.x <= (ch1.x + 50) && p1.x >= (ch1.x - 50) && p1.y <= (ch1.y + 50) && p1.y >= (ch1.y - 50) && chevalEna == 0 && chevalApp == 0)
+{
+  chevalApp = 1;
+  ch1.x = p1.x;
+  ch1.y = p1.y;
+  chevalEna = 1;
+  ch1.v = 10;
+  p1.v=10;
+}
+if(p1.x >= (ch1.x + 50) || p1.x <= (ch1.x - 50) || p1.y >= (ch1.y + 50) || p1.y <= (ch1.y - 50) )
+{
+  chevalApp = 0;
+}
+    
 
     requestAnimationFrame(anime);
 }
