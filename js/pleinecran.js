56fe125acef2306fee5a1f6ba5174546d84d5c73
yoni_iut_nice <31920049+yonea@users.noreply.github.com>
2019-01-10 14:58:55 +0100

    Add files via upload
    ajout version final dans le dossier Survivor

---
+++ js/pleinecran.js
@@ -0,0 +1,32 @@
+function twPleinEcran(_element) {
+      var monElement = _element||document.documentElement;
+      if (document.mozFullScreenEnabled) {
+	if (!document.mozFullScreenElement) {
+          monElement.mozRequestFullScreen();
+        } else {
+          document.mozCancelFullScreen();
+        }
+      }
+      if (document.fullscreenElement) {
+	if (!document.fullscreenElement) {
+          monElement.requestFullscreen();
+        } else {
+          document.exitFullscreen();
+        }
+      }
+      if (document.webkitFullscreenEnabled) {
+	if (!document.webkitFullscreenElement) {
+          monElement.webkitRequestFullscreen();
+        } else {
+          document.webkitExitFullscreen();
+        }
+      }
+      if (document.msFullscreenEnabled) {
+	if (!document.msFullscreenElement) {
+          monElement.msRequestFullscreen();
+        } else {
+          document.msExitFullscreen();
+        }
+      }
+	  
+}
